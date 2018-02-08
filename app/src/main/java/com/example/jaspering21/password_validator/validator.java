c5c9ed7403febb26c659cfbeab84c493ec3d4152
jaspering21 <jasperingclearlove@gmail.com>
2018-02-08 18:24:26 -0400

    Implement two basic rules and another three new rules

--- app/src/main/java/com/example/jaspering21/password_validator/validator.java
+++ app/src/main/java/com/example/jaspering21/password_validator/validator.java
@@ -1,5 +1,7 @@
 package com.example.jaspering21.password_validator;
 
+import static android.view.View.Z;
+
 /**
  * Created by Jaspering21 on 2018-02-07.
  */
@@ -9,20 +11,21 @@ public class validator {
     private int count;
     public int checkValid(String password){
         word = password;
+        count = 0;
+        if(!word.equals("password")&&word.length()>8) {
+            count ++;
+        }
         if(word.length()>8) {
-            count = 1;
+            count ++;
         }
-        if (word.matches("[0-9]*")&&word.length()>8) {
-            count =2;
+        if (word.matches(".*[a-z].*")&&word.length()>8) {
+            count ++;
         }
-        if (word.matches("[a-z]*")&&word.matches("[0-9]*")&&word.length()>8) {
-            count = 3;
+        if (word.matches(".*[0-9].*")&&word.length()>8) {
+            count ++;
         }
-        if (word.matches("[!@#$%^&*()<>,./?]*")&&word.matches("[a-z]*")&&word.matches("[0-9]*")&&word.length()>8) {
-            count = 4;
-        }
-        if(word.equals("password")){
-            return 0;
+        if (word.matches(".*[A-Z].*")&&word.length()>8){
+            count ++;
         }
         return count;
     }
